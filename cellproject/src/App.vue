75b94ce986fee08fb0068e1643550c5439c6b032
Alexandre Dantas dos Santos <allexkid@info.ufrn.br>
2023-09-27 22:00:52 -0300

    Iniciando projeto.

--- cellproject/src/App.vue
+++ cellproject/src/App.vue
@@ -1,48 +1,30 @@
 <template>
-  <div id="app">
-    <Navbar :logo="logo_src" :alt="app_name"/>
-    <router-view/>
-    <Footer />
-  </div>
-  
+  <nav>
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </nav>
+  <router-view/>
 </template>
 
-<script>
-import Navbar from './components/Navbar.vue';
-import Footer from './components/Footer.vue';
-
-export default {
-  name: 'App',
-  components: {
-    Navbar,
-    Footer
-  },
-  data() {
-    return {
-      logo_src: '/img/files.png',
-      app_name: 'Papers Project'
-    }
-  }  
-}
-</script>
-
 <style>
-  * {
-    font-family: HELVETICA;
-    padding: 0;
-    margin: 0;
-    box-sizing: border-box;
-  }
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
 
-  .main-container{
-     margin: 50px;
-     min-height: 250px;
-  }
+nav {
+  padding: 30px;
+}
 
-  h1{
-    text-align: center;
-    font-size: 42px;
-    margin-bottom: 30px;
-    color: #222
-  }
+nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+nav a.router-link-exact-active {
+  color: #42b983;
+}
 </style>
