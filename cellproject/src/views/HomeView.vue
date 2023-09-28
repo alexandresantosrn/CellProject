7bbf952c4d4f16ba13cdd82cf7b81c497e903b94
Alexandre Dantas dos Santos <allexkid@info.ufrn.br>
2023-09-27 23:26:01 -0300

    Adicionando bootstrap.

--- cellproject/src/views/HomeView.vue
+++ cellproject/src/views/HomeView.vue
@@ -1,10 +1,13 @@
 <template>
 <div class="main-container">
-    <h1>Bem-vindo</h1>
-    
+  <h1>Ícones do Bootstrap em Vue.js</h1>
+
+  <p> asdasdasdas
+    <i class="bi bi-apple"></i></p>
+   
+  </div>
     
 
-  </div>
 </template>
 
 
