fb217eb34b61adf4f24367bc2f96af5b500ad5a0
Alexandre Dantas dos Santos <allexkid@info.ufrn.br>
2023-09-27 22:08:37 -0300

    Setup inicial.

--- cellproject/src/App.vue
+++ cellproject/src/App.vue
@@ -1,30 +1,48 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div id="app">
+    <Navbar :logo="logo_src" :alt="app_name"/>
+    <router-view/>
+    <Footer />
+  </div>
+  
 </template>
 
+<script>
+import Navbar from './components/Navbar.vue';
+import Footer from './components/Footer.vue';
+
+export default {
+  name: 'App',
+  components: {
+    Navbar,
+    Footer
+  },
+  data() {
+    return {
+      logo_src: '/img/files.png',
+      app_name: 'Papers Project'
+    }
+  }  
+}
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+  * {
+    font-family: HELVETICA;
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+  }
 
-nav {
-  padding: 30px;
-}
+  .main-container{
+     margin: 50px;
+     min-height: 250px;
+  }
 
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
+  h1{
+    text-align: center;
+    font-size: 42px;
+    margin-bottom: 30px;
+    color: #222
+  }
 </style>
--- cellproject/src/views/HomeView.vue
+++ cellproject/src/views/HomeView.vue
@@ -1,18 +1,9 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+<div class="main-container">
+    <h1>Bem-vindo</h1>
+    
+
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
-export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
-}
-</script>
